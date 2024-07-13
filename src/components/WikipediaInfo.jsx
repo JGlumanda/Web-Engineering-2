0ee96b29f907dd2678f3b9fd7d7096b994e38d2d
JGlumanda <jonas-berger@hotmail.com>
2024-07-13 23:19:20 +0200

    - Enhanced Sheet Modal

--- src/components/WikipediaInfo.jsx
+++ src/components/WikipediaInfo.jsx
@@ -1,29 +1,30 @@
-// WikipediaInfo.jsx
 import React from 'react';
-import { Sheet, Page, Block, Button, Icon, Toolbar } from 'framework7-react';
+import {Block, Button, Icon, Page, Sheet, Toolbar} from 'framework7-react';
 import '../css/WikipediaInfo.css'; // Import the CSS file
 
-const WikipediaInfo = ({ query, opened, onClose, info }) => {
+const WikipediaInfo = ({query, opened, onClose, info}) => {
     return (
         <Sheet
-            className="wikipedia-sheet"
+            className="my-sheet-swipe-to-step wikipedia-sheet"
             opened={opened}
             backdrop={false}
             onSheetClosed={onClose}
+            swipeToClose
+            swipeToStep
         >
-            <Toolbar>
-                <div className="left wikipedia-sheet-title">{query}</div>
-                <div className="right">
-                    <Button small fill sheetClose onClick={onClose} className="wikipedia-sheet-close-button">
-                        <Icon f7="xmark" size="24px" />
-                    </Button>
-                </div>
-            </Toolbar>
-            <Page>
-                <Block className="wikipedia-sheet-content">
-                    <div>
-                        {info}
+            <div className="sheet-modal-swipe-step">
+                <Toolbar>
+                    <div className="left"><h1>{query}</h1></div>
+                    <div className="right">
+                        <Button small fill sheetClose onClick={onClose} className="wikipedia-sheet-close-button">
+                            <Icon f7="xmark" size="24px"/>
+                        </Button>
                     </div>
+                </Toolbar>
+            </div>
+            <Page style={{padding: '16px'}}>
+                <Block >
+                    {info}
                 </Block>
             </Page>
         </Sheet>
